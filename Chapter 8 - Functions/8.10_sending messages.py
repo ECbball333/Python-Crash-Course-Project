20bdf8357d65de1163f266d1a946ccff8bb0e34e
elliotconner <elliot.conner@icloud.com>
2025-10-29 15:48:03 -0400

    Challenges 8.9-8.11

---
+++ Chapter 8 - Functions/8.10_sending messages.py
@@ -0,0 +1,23 @@
+
+phrases = ["get over here!", "it's a home run!", "strike three you're out!"]
+new_received_messages = []
+
+def send_messages(old_phrases, sent_messages):
+    """ Print phrases and remove from phrases list"""
+    while phrases:
+        new_phrase = phrases.pop()
+        print(f"\nThe phrase: ({new_phrase}) has been moved.")
+        new_received_messages.append(new_phrase)
+
+def received_messages(new_received_messages):
+    """Show completed messages added to sent_messages"""
+    for message in new_received_messages:
+        print("\n--- Incoming Message---")
+        print(f"The following phrase has been received: \n\t{message}")
+
+send_messages(phrases, received_messages)
+received_messages(new_received_messages)
+
+#Print Lists to prove items have been moved to the other list
+print(f"\n{new_received_messages}")
+print(f"\n{phrases}")
---
+++ Chapter 8 - Functions/8.9_messages.py
@@ -0,0 +1,8 @@
+phrases = ['get over here!', 'happy new year!', 'merry christmas!']
+
+def show_messages(phrases):
+    """ Print phrases"""
+    for phrase in phrases:
+        print(f"{phrase.title()}")
+
+show_messages(phrases)
